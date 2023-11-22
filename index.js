b52408d70091d8a14cacaef56d2354fee666ec9b
liamjsmith <liamjs100@gmail.com>
2023-11-22 12:14:59 +0000

    pulling through required info from hacker news

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const url = 'https://news.ycombinator.com/'
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
-    })
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.athing', html).each(function() {
+            const title = $(this).find('.title .titleline').text()
+            const rank = $(this).find('.rank').text()
+            const points = $(this).next().find('.score').text()
+            const comments = $(this).next().find('.subtext a:contains("comment")').text() 
+            articles.push({
+                title,
+                rank,
+                comments: parseInt(comments) || 0,
+                points: parseInt(points) || 0,
+            })
+            return articles.slice(0, 30);
+        })
+        console.log(articles)
+    }).catch(err => console.log(err)) 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
