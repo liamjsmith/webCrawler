1ea744098736f5920df33063aca8b7802bcc5269
liamjsmith <liamjs100@gmail.com>
2023-11-22 13:52:54 +0000

    installed cors package to bring crawled data through

--- index.js
+++ index.js
@@ -2,31 +2,39 @@ const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 const url = 'https://news.ycombinator.com/'
 
+app.get('/', function (req, res) {
+    res.json('This is my webscraper')
+})
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.athing', html).each(function() {
-            const title = $(this).find('.title .titleline').text()
-            const rank = $(this).find('.rank').text()
-            const points = $(this).next().find('.score').text()
-            const comments = $(this).next().find('.subtext a:contains("comment")').text() 
-            articles.push({
-                title,
-                rank,
-                comments: parseInt(comments) || 0,
-                points: parseInt(points) || 0,
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
+            
+            $('.athing', html).each(function() {
+                const title = $(this).find('.title .titleline').text()
+                const rank = $(this).find('.rank').text()
+                const points = $(this).next().find('.score').text()
+                const comments = $(this).next().find('.subtext a:contains("comment")').text() 
+                articles.push({
+                    title,
+                    rank,
+                    comments: parseInt(comments) || 0,
+                    points: parseInt(points) || 0,
+                })
+                return articles.slice(0, 30);
             })
-            return articles.slice(0, 30);
-        })
-        console.log(articles)
-    }).catch(err => console.log(err)) 
+            res.json(articles)
+            console.log(articles)
+        }).catch(err => console.log(err)) 
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
