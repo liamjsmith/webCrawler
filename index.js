d7d6df9a8886aa679e6945bc0910abbc4b8540f7
liamjsmith <liamjs100@gmail.com>
2023-11-22 13:02:38 +0000

    pulling the data through to the frontend to filter

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const PORT = 8000
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const url = 'https://news.ycombinator.com/'
+const url = 'https://news.ycombinator.com/';
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.athing', html).each(function() {
-            const title = $(this).find('.title .titleline').text()
-            const rank = $(this).find('.rank').text()
-            const points = $(this).next().find('.score').text()
-            const comments = $(this).next().find('.subtext a:contains("comment")').text() 
-            articles.push({
-                title,
-                rank,
-                comments: parseInt(comments) || 0,
-                points: parseInt(points) || 0,
-            })
-            return articles.slice(0, 30);
-        })
-        console.log(articles)
-    }).catch(err => console.log(err)) 
+app.get('/scrape', async (req, res) => {
+  try {
+    const response = await axios(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+    $('.athing', html).each(function () {
+      const title = $(this).find('.title .titleline').text();
+      const rank = $(this).find('.rank').text();
+      const points = $(this).next().find('.score').text();
+      const comments = $(this).next().find('.subtext a:contains("comment")').text();
+      articles.push({
+        title,
+        rank,
+        comments: parseInt(comments) || 0,
+        points: parseInt(points) || 0,
+      });
+    });
+
+    const topArticles = articles.slice(0, 30);
+    res.json(topArticles);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
