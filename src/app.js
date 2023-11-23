d768de693326a4645232953f127439ba52992992
liamjsmith <liamjs100@gmail.com>
2023-11-23 11:57:22 +0000

    adding original data function for filters

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-const feedDisplay = document.querySelector('#feed')
+const feedDisplay = document.querySelector('#feed');
 
-fetch('http://localhost:8000/results')
-    .then(response => {return response.json()})
-    .then(data => {
-        data.forEach(article => {
-            const articleItem = `<div class="wrapper"><div class="titleContainer"><p>` + article.rank + `</p><h3>` + article.title + `</h3></div><div class="infoContainer"><p>` + article.points + `</p><p>` + article.comments + `</p></div></div>`
-            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
+let originalData = null;
+
+function fetchDataAndDisplay(data) {
+    feedDisplay.innerHTML = ''; // Clear previous content
+
+    data.forEach(article => {
+        const articleItem = `<div class="wrapper"><div class="titleContainer"><p>${article.rank}</p><a href="${article.url}"><p class="heading">${article.title}</p></a></div><div class="infoContainer"><p class="points">${article.points}</p><p class="comments">${article.comments}</p></div></div>`;
+        feedDisplay.insertAdjacentHTML('beforeend', articleItem);
+    });
+}
+
+function fetchOriginalData() {
+    fetch('http://localhost:8000/results')
+        .then(response => response.json())
+        .then(data => {
+            originalData = data;
+            fetchDataAndDisplay(originalData);
         })
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err));
+}
+
+    document.getElementById('filterByComments').addEventListener('click', () => {
+        const filteredData = originalData.filter(article => article.title.split(' ').length > 5);
+        const sortedData = filteredData.sort((a, b) => b.comments - a.comments);
+        fetchDataAndDisplay(sortedData);
+    });
+
+    document.getElementById('filterByPoints').addEventListener('click', () => {
+        const filteredData = originalData.filter(article => article.title.split(' ').length <= 5);
+        const sortedData = filteredData.sort((a, b) => b.points - a.points);
+        fetchDataAndDisplay(sortedData);
+    });
+
+    document.getElementById('reset').addEventListener('click', () => {
+        fetchDataAndDisplay(originalData);
+    });
+
+    // Initial data load
+    fetchOriginalData();
